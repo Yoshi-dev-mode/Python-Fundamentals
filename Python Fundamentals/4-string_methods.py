eff63fd889f3d81e38c07365f69949120b7ee870
Evangel Yoshiya G. Aranas <evanyoshi19@gmail.com>
2025-11-05 20:31:56 +0800

    Add files via upload

--- Python Fundamentals/4-string_methods.py
+++ Python Fundamentals/4-string_methods.py
@@ -3,18 +3,28 @@ STRING METHODS:
     1. string.captalize() - capitalized the first letter
     2. string.upper() - capitalized all of the letters
     3. string.lower() - lower case all of the letters
-    4. string.find() - finds the specific letter ["h","e","l","l","o"] --> [0,1,2,3,4]
-    5. string.casefold() - 
-    6. string.count()
-    7. string.startswith(""), ends with("")
-    8. string.center(width, symbol)
-    9. string.ljust(width, symbol)
-    10. string.rjust(width, symbol)
-    11. string.is___() - output: booleans
+    4. string.find() - finds the specific letter (-1 = not found) (0 above found)
+    ["h","e","l","l","o"] --> [0,1,2,3,4]
+    5. string.casefold() - stricker than lower case when it comes to unique symbol
+    6. string.count() - counts how many letter in a variable
+    7. string.startswith("") - True or False
+    8. string.endswith("") - True or False
+    9. string.center(width, symbol) - centers the strings
+    10. string.ljust(width, symbol) - left justify strings
+    11. string.rjust(width, symbol) - right justify strings
+    12. string.is___() - output: booleans
 
 """
 
-python_circuit = "hesllo world" 
-print(python_circuit.casefold())
+python_circuit = "com" 
 
+# OUTPUT 
+print(python_circuit)
+
+
+# DIFFERENCE BETWEEN lower() and casefold()
+text1 = "Straße" # ß -> ss = german word
+
+print(text1.lower())
+print(text1.casefold())  
 
---
+++ Python Fundamentals/5-input-and-formatting.py
@@ -0,0 +1,12 @@
+"""
+input() - is used to get input from the user during program execution. it always return a string
+
+"""
+
+name = input("What is your name: ")
+print(f"Hello {name} ")
+
+
+
+
+
