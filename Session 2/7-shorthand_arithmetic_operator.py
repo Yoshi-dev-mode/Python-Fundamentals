aee21bac137414c202f14cd19b24ec008e0378d0
Evangel Yoshiya G. Aranas <evanyoshi19@gmail.com>
2025-11-09 08:18:31 +0800

    Add files via upload

---
+++ Session 2/7-shorthand_arithmetic_operator.py
@@ -0,0 +1,28 @@
+"""
+SHORTHAND ARITHMETIC OPERATORS:
+    1. += - Add and assign
+    2. -= - Subtract and assign
+    3. *= - Multiply and assign
+    4. /= - Divide and assign
+    5. **= - Exponentiate and assign
+    6. //= - Floor divide and assign
+    7. %= - Modulus and assign
+    
+"""
+
+container = 5 # --> 3
+coke_stock_number = 3
+mountain_dew_stock_number = 4
+
+# USE THIS:
+# container += coke_stock_number # 3 stocks
+# container += mountain_dew_stock_number # 7 stocks
+# print(f"You got {container} stock!")
+
+
+
+# # INSTEAD OF THIS:
+# container =  coke_stock_number + container # 0 + 3 = 3 stocks 
+# container = container + mountain_dew_stock_number # 3 + 4 = 7 stocks
+# print(f"You got {container} stocks!")
+
