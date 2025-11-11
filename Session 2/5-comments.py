c4606208d61e1623784bf253e57b988d6baa68bf
Evangel Yoshiya G. Aranas <evanyoshi19@gmail.com>
2025-11-11 08:37:18 +0800

    Add files via upload

--- Session 2/5-comments.py
+++ Session 2/5-comments.py
@@ -1,12 +1,22 @@
 # 2 types of comments:
 # 1. Single Line comments 
+
 """
 2. Multiline comments
 
 """
+
+"""
+STEPS SHORTCUT KEYS FOR COMMENT:
+    1. Select all of the code you want to comment
+    2. Ctrl + /
+    
+"""
+
 
 # Best way to comment:
 
 # This prints something ❌
 # Message after the user logs in ✅
 print("Login Successful")
+
--- Session 2/6-string_methods.py
+++ Session 2/6-string_methods.py
@@ -4,16 +4,25 @@ STRING METHODS:
     2. string.upper() - capitalized all of the letters
     3. string.lower() - lower case all of the letters
     4. string.find() - finds the specific letter (-1 = not found) (0 above =found)
-    ["h","e","l","l","o"] --> [0,1,2,3,4]
-    5. string.casefold() - stricker than lower case when it comes to unique symbol
+    ["h","e","l","l","o"] --> [0,1,,3,4]
+    5. string.casefold() - stricter than lower case when it comes to unique symbol
     6. string.count() - counts how many letter in a variable
     7. string.startswith("") - True or False
     8. string.endswith("") - True or False
     9. string.center(width, symbol) - centers the strings
     10. string.ljust(width, symbol) - left justify strings
     11. string.rjust(width, symbol) - right justify strings
-    12. string.is___() - output: booleans
-
+    12. string.isalpha() - “Is it only letters and there's no number?”
+    13. string.isdigit() - “Is it only digits?”
+    14. string.isalnum() - “Is it letters or numbers?”
+    15. string.islower() - “Is it all lowercase?”
+    16. string.isupper() - “Is it all uppercase?”
+    17. string.istitle() - “Is it title case?” False if the variable if empty and all of the first words is not capitalized
+    18. string.isspace() - “Is it only whitespace?”
+    19. string.isprintable() - “Can it be printed?” False if there's \n and \t
+    20. string.isascii() - “Is it ASCII?” What is ASCII (American Standard Code for Information Interchange)? 
+                         - Computers know numbers not letters
+    21. string.isidentifier() - "Is it a name used for variables, functions, classes, etc.?"
 """
 
 """
@@ -21,13 +30,22 @@ STRING METHODS:
     \t - tab line
 """
 
-python_circuit = "com" 
+
+
+# print(ord(char))
+
+"""
+["h","e","l","l","o"] --> [0,1,,3,4]
+
+"""
+
+
 
 # DIFFERENCE BETWEEN lower() and casefold()
-text1 = "Straße" # ß -> ss = german word
+# text1 = "Straße" # ß -> ss = german word()
 
-print(text1.lower())
-print(text1.casefold())  
+# print(text1.upper())
+# print(text1.casefold())  
 
 
 
